652bc0f953e681bc9f67c40f262e84460d353a3b
Vadim <79119169948@yandex.ru>
2022-07-10 12:57:16 +0300

    add 001

---
+++ 001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Объявление переменных
+// int a,b=0;
+// double c;
+// string s;
+int a=2,b=3;
+int s=a+b;
+Console.WriteLine(a+"+"+b+"="+s); // склеивание строк
+Console.WriteLine("{0}+{1}={2}",a,b,s); // строка форматирования
+Console.WriteLine("{2}+{1}={0}",s,b,a);
+Console.WriteLine($"{a}+{b}={s}"); // строка интерполяции
