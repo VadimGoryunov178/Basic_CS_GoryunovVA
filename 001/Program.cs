2bf05bc2ceb438968d2e826e3d0a6f637d1ed00d
Vadim <79119169948@yandex.ru>
2022-07-10 13:32:41 +0300

    решение задач

--- 001/Program.cs
+++ 001/Program.cs
@@ -2,9 +2,31 @@
 // int a,b=0;
 // double c;
 // string s;
-int a=2,b=3;
+/* Многострочный комментарий
+int a=2,b=3; // Операция присваивания
 int s=a+b;
 Console.WriteLine(a+"+"+b+"="+s); // склеивание строк
 Console.WriteLine("{0}+{1}={2}",a,b,s); // строка форматирования
 Console.WriteLine("{2}+{1}={0}",s,b,a);
 Console.WriteLine($"{a}+{b}={s}"); // строка интерполяции
+*/
+
+// Вывести квадрат числа (Ввести с клавиатуры целое число и вывести квадрат этого числа на экран)
+
+/*
+int a;
+int b;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a*a;
+System.Console.WriteLine($"{a}^2={b}");
+*/
+// Для не целого числа
+double a;
+double b;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+b=a*a;
+System.Console.WriteLine($"{a}^2={b}");
