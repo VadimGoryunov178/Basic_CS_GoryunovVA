45292cb84b3c3d2d93026a4f83d3a6d47aed4ada
Vadim <79119169948@yandex.ru>
2022-07-12 13:51:05 +0300

    решение 5 задачи

---
+++ 005/Program.cs
@@ -0,0 +1,19 @@
+﻿// С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+int a;
+int b;
+int c;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+s=Console.ReadLine();
+c=Convert.ToInt32(s);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+ 
+Console.Write("max= ");
+Console.WriteLine(max);
