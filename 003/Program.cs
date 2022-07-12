df7758c3c36f816bd1218437b97011c8f77151e8
Vadim <79119169948@yandex.ru>
2022-07-12 12:54:14 +0300

    Решение 3 задачи

---
+++ 003/Program.cs
@@ -0,0 +1,17 @@
+﻿// С клавиатуры вводятся два числа a и b. 
+// Найти максимальное из них.
+int a;
+int b;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+
+if (a>b)
+{
+    System.Console.WriteLine("a");
+}
+else
+{
+    System.Console.WriteLine($"{b} ");
+}
