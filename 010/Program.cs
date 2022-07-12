30857ca977909db23bea37a897ab60e71ee7a126
Vadim <79119169948@yandex.ru>
2022-07-12 14:24:08 +0300

    Решение 10 задачи

---
+++ 010/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вывести на экран последнюю цифру целого числа 
+// введенного с клавиатуры. 
+
+int a;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+System.Console.WriteLine($"{a%10}");
