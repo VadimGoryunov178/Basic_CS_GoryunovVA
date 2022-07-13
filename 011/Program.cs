cc1befc5f8832ee078b83afc0aa806dd717bf666
Vadim <79119169948@yandex.ru>
2022-07-13 13:10:17 +0300

    Решение задачи 011

---
+++ 011/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число больше 9. Вывести на экран  вторую цифру числа 
+// с конца
+int a=Convert.ToInt32(Console.ReadLine());
+if(a>9)
+{ //1234
+    int b=a/10;
+    int c=b%10;
+    System.Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine("Ошибка.Число меньше 9");
+}
