02eb454ee1be66049842b9cc05fe793ba06a766a
Vadim <79119169948@yandex.ru>
2022-07-13 11:27:47 +0300

    Решение задачи №12

---
+++ 012/Program.cs
@@ -0,0 +1,11 @@
+﻿// С клавиатуры вводится целое число из  диапазона  [10, 99]. 
+// Показать наибольшую цифру числа
+
+int a;
+string? s;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+int d1=a/10;
+int d2=a%10;
+int m=Math.Max(d1,d2);
+Console.WriteLine(m);
