961e79f299e5f6a54287f895d09112adf9779a01
Vadim <79119169948@yandex.ru>
2022-07-13 12:52:11 +0300

    Решение 007 задачи

---
+++ 007/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным.
+int a=Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
