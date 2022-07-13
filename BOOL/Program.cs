096edec18dbeddc761848f11411042390beda6a5
Vadim <79119169948@yandex.ru>
2022-07-13 22:11:57 +0300

    Описание Bool

--- BOOL/Program.cs
+++ BOOL/Program.cs
@@ -1,6 +1,6 @@
-﻿
+﻿/*
 int a = 100;
-float f = 3.14F;
+// float f = 3.14F;
 double f = 3.14;
 bool b;
 b=a>50; //  простое условие >,<,>=,<=,==,!=
@@ -10,4 +10,25 @@ if (b)
 {
 
 }
+*/
+
 // Не, И, Или, Исключающее Или
+// Не (переворачивает)
+bool passed = false;
+// Console.WriteLine(!passed); // вывод True
+// Console.WriteLine(!true); // вывод False\
+// И
+//Console.WriteLine(false && false );
+//Console.WriteLine(false && true );
+//Console.WriteLine(true && false );
+//Console.WriteLine(true && true );
+// Или
+//Console.WriteLine(false || false );
+//Console.WriteLine(false || true );
+//Console.WriteLine(true || false );
+//Console.WriteLine(true || true );
+// Исключающее или
+Console.WriteLine(false ^ false );
+Console.WriteLine(false ^ true );
+Console.WriteLine(true ^ false );
+Console.WriteLine(true ^ true );
