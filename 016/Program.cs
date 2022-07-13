5aee80526142962460a6d61224d9d406c261ee3a
Vadim <79119169948@yandex.ru>
2022-07-13 22:27:47 +0300

    Решение задачи 16

---
+++ 016/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+bool b=a%7==0 && a%23==0;
+Console.WriteLine(b);
+if(b)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно");
+}
