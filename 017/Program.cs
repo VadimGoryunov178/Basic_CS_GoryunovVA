af6f5a1c6352f80ea39a5d891804acd7dd2058b4
Vadim <79119169948@yandex.ru>
2022-07-14 10:18:46 +0300

    Решение задачи 17

---
+++ 017/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число обозначающее день недели. 
+// Выяснить является номер дня недели выходным
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 1: Console.WriteLine("Понедельник - Будни"); break;
+    case 2: Console.WriteLine("Вторник - Будни"); break;
+    case 3: Console.WriteLine("Среда - Будни"); break;
+    case 4: Console.WriteLine("Четверг - Будни"); break;
+    case 5: Console.WriteLine("Пятница - Будни"); break;
+    case 6: Console.WriteLine("Суббота  - Выходной"); break;
+    case 7: Console.WriteLine("Воскресенье - Выходной"); break;
+    default: Console.WriteLine("ERROR"); break;
+}
