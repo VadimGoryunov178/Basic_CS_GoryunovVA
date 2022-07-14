545024dddc8c277f8854337ccccc5cd1de06caef
Vadim <79119169948@yandex.ru>
2022-07-14 11:18:15 +0300

    Решение 29 задачи

---
+++ 029/Program.cs
@@ -0,0 +1,16 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine(Numbers(455)); // Программа
+
+
+
+int Numbers(int N) // Подпрограмма
+{
+    int k=0;
+    while(N>0)
+    {
+    k=k + N%10;
+    N=N/10;
+    }
+    return k;
+}
