7b00b7798e7bd963dc633f3166a2afa54a14c7aa
Vadim <79119169948@yandex.ru>
2022-07-17 19:41:26 +0300

    справление 14 задачи

--- 014/Program.cs
+++ 014/Program.cs
@@ -2,11 +2,12 @@
 //Выяснить, кратно ли число a числу b, если нет, 
 //вывести остаток от деления a на b.
 
-Console.WriteLine("Введите два числа");
+Console.WriteLine("Введите первое число");
 int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
 int b=Convert.ToInt32(Console.ReadLine());
 int c=a/b;
-if(c%2==0)
+if(c==0)
 {
     Console.WriteLine("Числа кратны");
 }
