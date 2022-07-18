757178f96b0aa2a24c32d286c798c9dcecef0d5a
Vadim <79119169948@yandex.ru>
2022-07-18 22:03:40 +0300

    Решение задачи 34

--- 004/Program.cs
+++ 004/Program.cs
@@ -1,6 +1,7 @@
 ﻿// По заданному с клавиатуры номеру дня недели 
 // вывести его название
-
+// 1 способ
+/*
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 switch (a)
@@ -14,3 +15,24 @@ switch (a)
     case 7: Console.WriteLine("Воскресенье"); break;
     default: Console.WriteLine("ERROR"); break;
 }
+*/
+// 2 способ
+
+Console.Write("Введите число: ");
+//int dayOfWeek; //camelCase
+//int DaeOfcase; // PasсalCase (название методов подимен классов)
+double a = Convert.ToInt32(Console.ReadLine());
+//dayOfWeek=int.Parse(Console.ReadLine());
+if(a==1) Console.WriteLine("Пн");
+if(a==2) Console.WriteLine("Вт");
+if(a==3) Console.WriteLine("Ср");
+if(a==4) Console.WriteLine("Чт");
+if(a==5) Console.WriteLine("Пт");
+if(a==6) Console.WriteLine("Сб");
+if(a==7) Console.WriteLine("Вс");
+else 
+{
+    Console.WriteLine("Такого дня не существует");
+}
+
+
