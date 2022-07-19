5b7cf3885e91f6f396e0a07ef9d6cd472a1e06fc
Vadim <79119169948@yandex.ru>
2022-07-19 22:04:32 +0300

    ршение задачи 41

---
+++ 041/Program.cs
@@ -0,0 +1,32 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу 
+//между максимальным и минимальным элементом
+int N=8;
+int[] a=new int[N];
+Random random=new Random();//псевдослучайные числа
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(1,136);
+
+
+int max=0;
+for(int i=0;i<a.Length;i++)
+{
+   if (a[i]>max)
+    {
+        max= a[i];
+    }
+}
+int min=max;
+for(int i=0;i<a.Length;i++)
+{
+    if (a[i]<min)
+    {
+        min=a[i];
+    }
+    
+}
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+//System.Console.WriteLine();
+System.Console.WriteLine($"\nМаксимальеое число:{max}");
+System.Console.WriteLine($"Минимальное число:{min}");
+System.Console.WriteLine($"Разница:{max-min}");
