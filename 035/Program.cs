9e502aa417946ff9c93a032f5fe9f9034934a8cc
Vadim <79119169948@yandex.ru>
2022-07-19 23:15:09 +0300

    не полное решение задачи 35

---
+++ 035/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу замены элементов массива 
+//на противоположные
+
+int N=8;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+{
+    a[i]=random.Next(-9,10);
+}
+int opposite=0;
+for(int i=0;i<a.Length;i++)
+opposite = a[i]*(-1);
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+System.Console.WriteLine($"\nПротивоположно:{opposite}");
+  
