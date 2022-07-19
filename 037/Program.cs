379e8ff3f979bc10081d6f7c4a17105880ce5e44
Vadim <79119169948@yandex.ru>
2022-07-19 21:19:37 +0300

    ршение задачи 37

---
+++ 037/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать массив, заполнить случайными положительными 
+//трёхзначными числами. Показать количество нечетных\четных 
+//чисел
+int N=8;
+int[] a=new int[N];
+Random random=new Random();//псевдослучайные числа
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(99,1000);
+
+int evenNum=0;
+for(int i=0;i<a.Length;i++)
+{
+    if (a[i]%2==0)
+    {
+        evenNum++;
+    }
+}
+int oddNum=0;
+for(int i=0;i<a.Length;i++)
+{
+    if (a[i]%2>0)
+    {
+        oddNum++;
+    }
+}
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+//System.Console.WriteLine();
+System.Console.WriteLine($"\nКоличество четных:{evenNum}");
+System.Console.WriteLine($"Количество не четных:{oddNum}");
