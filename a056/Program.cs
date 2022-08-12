70fb41b93d81227c2a181dab081af04be060a88c
Vadim <79119169948@yandex.ru>
2022-08-12 21:17:05 +0300

    решение задачи 056

---
+++ a056/Program.cs
@@ -0,0 +1,43 @@
+﻿// Написать программу, которая обменивает элементы 
+//первой строки и последней 
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();  
+            const int n = 4, m = 4;    
+            int[,] matrix = new int[n, m]; 
+            int i, j, temp;   
+            Console.WriteLine("Рандомный массив:");
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    matrix[i, j] = rnd.Next(100); 
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        
+            for (j = 0; j < m; j++)
+            {
+                temp=matrix[0, j];
+                matrix[0, j]=matrix[n - 1, j];
+                matrix[n - 1, j] = temp;
+            }
+            Console.WriteLine("Массив после замены строк:");
+        
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+                Console.Read();
+        }
+    }
