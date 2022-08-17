174c54c8e40edd16fbb0c53080d50b65f35d782a
Vadim <79119169948@yandex.ru>
2022-08-17 22:12:14 +0300

    string03

--- string03/Program.cs
+++ string03/Program.cs
@@ -1,7 +1,8 @@
 ﻿// С клавиатуры вводится строка разделенная точкой. /
 //Подсчитать количество символов до точки
 using System;
+Console.WriteLine("Введите строку с точкой");
     string s = Console.ReadLine();
     int i = s.IndexOf('.');
  
-    Console.WriteLine($"До точки {i}");
+    Console.WriteLine($"До точки {i} символов");
