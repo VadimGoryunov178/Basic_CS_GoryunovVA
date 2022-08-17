0aa280a475c2b38e6f0aa9509fc88ea72cfa9a4e
Vadim <79119169948@yandex.ru>
2022-08-17 22:15:02 +0300

    string05

---
+++ string05/Program.cs
@@ -0,0 +1,27 @@
+﻿// Определить являются ли введенные с клавиатуры символы 
+//правильно записью целого числа. Вычислить сумму цифр 
+//введенного числа
+
+using System;
+ 
+namespace SharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Введите символы");
+            string num = Console.ReadLine();
+            int temp = 0;
+            if (!Int32.TryParse(num, out temp))
+                Console.WriteLine("Это не является целым числом");
+            else {
+                int sum = 0;
+                foreach (char digit in num)
+                    sum += digit - 48;
+                    Console.WriteLine("Сумма введеных чисел");
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
