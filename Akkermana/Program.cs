0d9e125d7609fac3614f136f41c709b676fdfd59
Vadim <79119169948@yandex.ru>
2022-08-17 21:32:37 +0300

    Akkermana

---
+++ Akkermana/Program.cs
@@ -0,0 +1,19 @@
+﻿//Написать программу вычисления функции Аккермана
+
+
+Console.WriteLine("Введите число n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число m: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            A(n, m);
+            
+            static int A(int n, int m)
+            {
+                if (n == 0) return m + 1;
+                if (n != 0 && m == 0) return A(n - 1, 1);
+                if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+                return A(n,m);
+                
+            }
+            Console.WriteLine(A);
+            
