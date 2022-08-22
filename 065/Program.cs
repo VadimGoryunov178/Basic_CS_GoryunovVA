b8e6201c5c0cd806c7b3aabd52ed5762907a2f20
Vadim <79119169948@yandex.ru>
2022-08-22 20:17:05 +0300

    065

---
+++ 065/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке 
+//от M до N.
+
+int n, m, s=0;
+Console.Write("Введи первое число: ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи последнее число: ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = n; i <= m; i++)
+    s += i;
+Console.Write("Сумма чисел в заданном промежутке= ");
+Console.WriteLine(s);
